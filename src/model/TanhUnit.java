d71e7236bd056e38cf7abd9d4ac863c6578c79fd
mahat <mehmet.ahat@gmail.com>
2016-05-01 01:37:44 +0300

    derivative of tanh is updated

--- src/model/TanhUnit.java
+++ src/model/TanhUnit.java
@@ -1,20 +1,26 @@
 package model;
 
 
-
 public class TanhUnit implements Nonlinearity {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	public double forward(double x) {
-		return Math.tanh(x);
-	}
+    @Override
+    public double forward(double x) {
+        return Math.tanh(x);
+    }
 
-	@Override
-	public double backward(double x) {
-		double coshx = Math.cosh(x);
-		double denom = (Math.cosh(2*x) + 1);
-		return 4 * coshx * coshx / (denom * denom);
-	}
+    @Override
+    public double backward(double x) {
+        /*
+        * Old Code
+		* double coshx = Math.cosh(x);
+		* double denom = (Math.cosh(2*x) + 1);
+		* return 4 * coshx * coshx / (denom * denom);
+		* */
+
+        // Using basic tanh derivative
+        return 1 - (Math.tanh(x) * Math.tanh(x));
+
+    }
 }
